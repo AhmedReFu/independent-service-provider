20e53b8bb249d71ec94061a85fe2e6c4c51cd00c
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-05-11 04:10:26 +0600

    About done

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
 
                 <div>
                     <h1>MD. NAZMUL ISLAM</h1>
-                    <p className='fs-4 mt-5'>I have 2 years of experience on the web. First I started learning HTML, CSS  and JS then I create some simple websites that are single and different pages. I have already learned 80% HTML,  60% CSS and 50% JS. I can build any simple website in html css and js. I can even teach my grandmother how to create simple websites. My goal is to become an honest and good fullstack web developer.</p>
+                    <p className='fs-3 mt-5'>I have 2 years of experience on the web. First I started learning HTML, CSS  and JS then I create some simple websites that are single and different pages. I have already learned 80% HTML,  60% CSS and 50% JS. I can build any simple website in html css and js. I can even teach my grandmother how to create simple websites. My goal is to become an honest and good fullstack web developer.</p>
                 </div>
             </div>
         </div>
