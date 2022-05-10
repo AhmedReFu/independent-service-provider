65228a1cca1ff2ce2adb75f34d0696a251e2a45e
AhmedReFu <mdnazmulkhanfeni@gmail.com>
2022-05-11 04:24:36 +0600

    require auth some condition added

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
@@ -11,9 +12,11 @@ const RequireAuth = ({ children }) => {
         return <Loading></Loading>
     }
     if (!user) {
+        toast('Please Login Then You Enteier This Part.')
         return <Navigate to="/login" state={{ from: location }} replace />;
     }
     return children;
+
 };
 
 export default RequireAuth;
